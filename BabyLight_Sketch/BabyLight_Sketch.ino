5d868d331e42bd40b6747c439cc650dfe054a07e
schwabfour@gmail.com <schwabfour@gmail.com>
2022-03-03 17:39:38 -0500

    updated configuration setup

--- BabyLight_Sketch/BabyLight_Sketch.ino
+++ BabyLight_Sketch/BabyLight_Sketch.ino
@@ -1,9 +1,13 @@
+#include <Adafruit_NeoPixel.h>
+int led = 1; //digital pin 1 (Red LED on the Trinket)
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(led,OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH);
+  delay(500);
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BabyLight
 Light for the Baby's Room
+
+Arduino IDE 
+https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
+
+Bootloader
+USBTinyISP
+
+Hardware List
+USB2.0 to USB Micro (communications..not power-only ;) )
+Adafruit Pro Trinket 5V/16MHz (USB), Part# P2000
