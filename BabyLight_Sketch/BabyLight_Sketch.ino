fdc2b01ac0016c7cfb3a047fe3efa9faf589ac0b
schwabfour@gmail.com <schwabfour@gmail.com>
2022-03-04 14:49:21 -0500

    first prototype

--- BabyLight_Sketch/BabyLight_Sketch.ino
+++ BabyLight_Sketch/BabyLight_Sketch.ino
@@ -1,13 +1,29 @@
 #include <Adafruit_NeoPixel.h>
-int led = 1; //digital pin 1 (Red LED on the Trinket)
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define PIN      5
+#define NUMPIXELS 7
+
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+
 void setup() {
-  pinMode(led,OUTPUT);
-  
+pixels.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(led,HIGH);
-  delay(500);
-
+pixels.clear();
+pixels.setBrightness(10);
+pixels.setPixelColor(0, pixels.Color(255, 255, 255));
+pixels.setPixelColor(1, pixels.Color(255, 0, 0));
+pixels.setPixelColor(2, pixels.Color(0, 255, 0));
+pixels.setPixelColor(3, pixels.Color(0, 0, 255));
+pixels.setPixelColor(4, pixels.Color(255, 0, 255));
+pixels.setPixelColor(5, pixels.Color(255, 255, 0));
+pixels.setPixelColor(6, pixels.Color(0, 255, 255));
+pixels.show();
 }
